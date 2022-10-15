fcce26791569c1c220ac6261412a5a9639fb0374
Félix Saparelli <felix@passcod.name>
2022-10-15 19:31:06 +1300

    Domain checks aren't isolated, it's a bit weird

--- migrations/000_base.sql
+++ migrations/000_base.sql
@@ -11,10 +11,12 @@ CREATE TYPE channel_t AS (
 
 CREATE DOMAIN channel AS channel_t
 CHECK (
-	(VALUE).guild_id IS NOT NULL
-	AND (VALUE).guild_id > 0
-	AND (VALUE).channel_id IS NOT NULL
-	AND (VALUE).channel_id > 0
+	VALUE IS NULL OR (
+		(VALUE).guild_id IS NOT NULL
+		AND (VALUE).guild_id > 0
+		AND (VALUE).channel_id IS NOT NULL
+		AND (VALUE).channel_id > 0
+	)
 );
 
 CREATE TYPE member_t AS (
@@ -23,16 +25,20 @@ CREATE TYPE member_t AS (
 );
 
 CREATE DOMAIN member AS member_t CHECK (
-	(VALUE).guild_id IS NOT NULL
-	AND (VALUE).guild_id > 0
-	AND (VALUE).user_id IS NOT NULL
-	AND (VALUE).user_id > 0
+	VALUE IS NULL OR (
+		(VALUE).guild_id IS NOT NULL
+		AND (VALUE).guild_id > 0
+		AND (VALUE).user_id IS NOT NULL
+		AND (VALUE).user_id > 0
+	)
 );
 
 CREATE TYPE message_t AS (channel channel, message_id int8);
 
 CREATE DOMAIN "message" AS message_t CHECK (
-	(VALUE).channel IS NOT NULL
-	AND (VALUE).message_id IS NOT NULL
-	AND (VALUE).message_id > 0
+	VALUE IS NULL OR (
+		(VALUE).channel IS NOT NULL
+		AND (VALUE).message_id IS NOT NULL
+		AND (VALUE).message_id > 0
+	)
 );
