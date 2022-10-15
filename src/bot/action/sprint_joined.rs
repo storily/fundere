89e7a9b47e9e9f768714f621a29bab32eea49283
Félix Saparelli <felix@passcod.name>
2022-10-15 21:21:27 +1300

    Use send_response for sprint join

--- src/bot/action/sprint_joined.rs
+++ src/bot/action/sprint_joined.rs
@@ -1,79 +1,56 @@
-use miette::{IntoDiagnostic, Result};
-use twilight_model::{
-	application::{
-		component::{button::ButtonStyle, Button, Component},
-		interaction::Interaction,
-	},
-	channel::message::MessageFlags,
-	http::interaction::{InteractionResponse, InteractionResponseType},
-	id::{marker::InteractionMarker, Id},
+use miette::Result;
+use twilight_model::application::{
+	component::{button::ButtonStyle, Button, Component},
+	interaction::Interaction,
 };
-use twilight_util::builder::InteractionResponseDataBuilder;
-use uuid::Uuid;
 
-use crate::{bot::utils::action_row, db::sprint::Sprint};
+use crate::{
+	bot::{
+		context::{GenericResponse, GenericResponseData},
+		utils::action_row,
+	},
+	db::sprint::Sprint,
+};
 
 use super::{Action, ActionClass, Args};
 
 #[derive(Debug, Clone)]
-pub struct SprintJoined {
-	pub id: Id<InteractionMarker>,
-	pub token: String,
-	pub sprint_id: Uuid,
-	pub shortid: i32,
-}
+pub struct SprintJoined(GenericResponse);
 
 impl SprintJoined {
 	#[tracing::instrument(name = "SprintJoined", skip(interaction))]
 	pub fn new(interaction: &Interaction, sprint: Sprint) -> Action {
-		ActionClass::SprintJoined(Self {
-			id: interaction.id,
-			token: interaction.token.clone(),
-			sprint_id: sprint.id,
-			shortid: sprint.shortid,
-		})
+		let Sprint { id, shortid, .. } = sprint;
+		ActionClass::SprintJoined(Self(GenericResponse::from_interaction(
+			interaction,
+			GenericResponseData {
+				ephemeral: true,
+				content: Some(format!("You've joined sprint `{shortid}`!")),
+				components: action_row(vec![
+					Component::Button(Button {
+						custom_id: Some(format!("sprint:start-words:{id}")),
+						disabled: false,
+						emoji: None,
+						label: Some("Record starting words".to_string()),
+						style: ButtonStyle::Primary,
+						url: None,
+					}),
+					Component::Button(Button {
+						custom_id: Some(format!("sprint:leave:{id}")),
+						disabled: false,
+						emoji: None,
+						label: Some("Leave".to_string()),
+						style: ButtonStyle::Danger,
+						url: None,
+					}),
+				]),
+				..Default::default()
+			},
+		)))
 		.into()
 	}
 
 	pub async fn handle(self, Args { app, .. }: Args) -> Result<()> {
-		let Self {
-			sprint_id, shortid, ..
-		} = self;
-		app.interaction_client()
-			.create_response(
-				self.id,
-				&self.token,
-				&InteractionResponse {
-					kind: InteractionResponseType::ChannelMessageWithSource,
-					data: Some(
-						InteractionResponseDataBuilder::new()
-							.content(format!("You've joined sprint `{shortid}`!"))
-							.flags(MessageFlags::EPHEMERAL)
-							.components(action_row(vec![
-								Component::Button(Button {
-									custom_id: Some(format!("sprint:start-words:{sprint_id}")),
-									disabled: false,
-									emoji: None,
-									label: Some("Record starting words".to_string()),
-									style: ButtonStyle::Primary,
-									url: None,
-								}),
-								Component::Button(Button {
-									custom_id: Some(format!("sprint:leave:{sprint_id}")),
-									disabled: false,
-									emoji: None,
-									label: Some("Leave".to_string()),
-									style: ButtonStyle::Danger,
-									url: None,
-								}),
-							]))
-							.build(),
-					),
-				},
-			)
-			.exec()
-			.await
-			.into_diagnostic()?;
-		Ok(())
+		app.send_response(self.0).await.map(drop)
 	}
 }
