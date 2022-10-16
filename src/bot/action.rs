a8a67f0b1d216a65dfafb1667793d41d131d7875
Félix Saparelli <felix@passcod.name>
2022-10-16 17:46:23 +1300

    Reduce repetitive work with a macro

--- src/bot/action.rs
+++ src/bot/action.rs
@@ -1,91 +1,59 @@
-use miette::Result;
+macro_rules! action {
+	(
+		$($modname:ident : $typename:ident),*
+	) => {
+		$(
+			pub mod $modname;
+			pub use self::$modname::$typename;
+		)*
 
-pub use self::calc_result::CalcResult;
-pub use self::command_ack::CommandAck;
-pub use self::command_error::CommandError;
-pub use self::sprint_announce::SprintAnnounce;
-pub use self::sprint_cancelled::SprintCancelled;
-pub use self::sprint_end::SprintEnd;
-pub use self::sprint_joined::SprintJoined;
-pub use self::sprint_left::SprintLeft;
-pub use self::sprint_start::SprintStart;
-pub use self::sprint_summary::SprintSummary;
-pub use self::sprint_update::SprintUpdate;
-pub use self::sprint_warning::SprintWarning;
-pub use self::sprint_words_end::SprintWordsEnd;
-pub use self::sprint_words_start::SprintWordsStart;
-
-use super::App;
-
-pub mod calc_result;
-pub mod command_ack;
-pub mod command_error;
-pub mod sprint_announce;
-pub mod sprint_cancelled;
-pub mod sprint_end;
-pub mod sprint_joined;
-pub mod sprint_left;
-pub mod sprint_start;
-pub mod sprint_summary;
-pub mod sprint_update;
-pub mod sprint_warning;
-pub mod sprint_words_end;
-pub mod sprint_words_start;
-
-#[derive(Debug, Clone)]
-pub struct Action {
-	pub class: ActionClass,
-}
-
-impl Action {
-	pub async fn handle(self, app: App) -> Result<()> {
-		let args = Args { app };
-
-		use ActionClass::*;
-		match self.class {
-			CalcResult(data) => data.handle(args).await,
-			CommandAck(data) => data.handle(args).await,
-			CommandError(data) => data.handle(args).await,
-			SprintAnnounce(data) => data.handle(args).await,
-			SprintCancelled(data) => data.handle(args).await,
-			SprintEnd(data) => data.handle(args).await,
-			SprintJoined(data) => data.handle(args).await,
-			SprintLeft(data) => data.handle(args).await,
-			SprintStart(data) => data.handle(args).await,
-			SprintSummary(data) => data.handle(args).await,
-			SprintUpdate(data) => data.handle(args).await,
-			SprintWarning(data) => data.handle(args).await,
-			SprintWordsStart(data) => data.handle(args).await,
-			SprintWordsEnd(data) => data.handle(args).await,
+		#[derive(Debug, Clone)]
+		pub struct Action {
+			pub class: ActionClass,
 		}
-	}
+
+		#[derive(Debug)]
+		pub struct Args {
+			pub app: super::App,
+		}
+
+		impl Action {
+			pub async fn handle(self, app: super::App) -> ::miette::Result<()> {
+				let args = Args { app };
+
+				use ActionClass::*;
+				match self.class {
+					$($typename(action) => action.handle(args).await),*
+				}
+			}
+		}
+
+		#[derive(Debug, Clone)]
+		pub enum ActionClass {
+			$($typename($typename)),*
+		}
+
+		impl From<ActionClass> for Action {
+			fn from(class: ActionClass) -> Self {
+				Self { class }
+			}
+		}
+	};
 }
 
-#[derive(Debug)]
-pub struct Args {
-	pub app: App,
-}
-
-impl From<ActionClass> for Action {
-	fn from(class: ActionClass) -> Self {
-		Self { class }
-	}
-}
-
-#[derive(Debug, Clone)]
-pub enum ActionClass {
-	CalcResult(CalcResult),
-	CommandAck(CommandAck),
-	CommandError(CommandError),
-	SprintAnnounce(SprintAnnounce),
-	SprintCancelled(SprintCancelled),
-	SprintEnd(SprintEnd),
-	SprintJoined(SprintJoined),
-	SprintLeft(SprintLeft),
-	SprintStart(SprintStart),
-	SprintSummary(SprintSummary),
-	SprintUpdate(SprintUpdate),
-	SprintWarning(SprintWarning),
-	SprintWordsStart(SprintWordsStart),
-	SprintWordsEnd(SprintWordsEnd),
-}
+action!(
+	calc_result : CalcResult,
+	command_ack : CommandAck,
+	command_error : CommandError,
+	sprint_announce : SprintAnnounce,
+	sprint_cancelled : SprintCancelled,
+	sprint_end : SprintEnd,
+	sprint_joined : SprintJoined,
+	sprint_left : SprintLeft,
+	sprint_start : SprintStart,
+	sprint_summary : SprintSummary,
+	sprint_update : SprintUpdate,
+	sprint_warning : SprintWarning,
+	sprint_words_end : SprintWordsEnd,
+	sprint_words_start : SprintWordsStart
+);
